86028214a308017f0eb92c2e4a9bd097974dd3e2
Rizki Ramadhan <66109920+korizki@users.noreply.github.com>
2022-11-02 17:27:34 +0800

    update terbaru

--- controllers/getSummary.php
+++ controllers/getSummary.php
@@ -1,20 +1,25 @@
 <?php 
     include 'connection.php';
-    $array_data = array();
-    $query = "SELECT * FROM tickets ORDER BY req_date DESC";
-    $result = mysqli_query($connection, $query);
-    while($row = mysqli_fetch_array($result)){
-        $array_data[] = array(
-            "id" => $row['ticket_id'],
-            "requestor" => $row['requestor'],
-            "sn_unit" => $row['sn_unit'],
-            "job" => $row['job_type'],
-            "req_date" => $row['req_date'],
-            "email" => $row['cp_email'],
-            "phone" => $row['cp_phone'],
-            "status" => $row['status']
+    session_start();
+    $user = $_SESSION['logged_user'];
+        $array_data = array();
+        $query = "SELECT * FROM tickets WHERE requestor = '$user' ORDER BY req_date DESC";
+        $result = mysqli_query($connection, $query);
+        while($row = mysqli_fetch_array($result)){
+            $array_data[] = array(
+                "id" => $row['ticket_id'],
+                "requestor" => $row['requestor'],
+                "sn_unit" => $row['sn_unit'],
+                "job" => $row['job_type'],
+                "req_date" => $row['req_date'],
+                "email" => $row['cp_email'],
+                "phone" => $row['cp_phone'],
+                "status" => $row['status']
+            );
+        }
+        echo json_encode($array_data);
+    // } else {
+    //     header("Location: ../index.php");
+    // }
 
-        );
-    }
-    echo json_encode($array_data);
 ?>
