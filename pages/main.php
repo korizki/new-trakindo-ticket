454b2ae6b6b6df1b8f08a6bddf09f16156e8da66
Rizki Ramadhan <66109920+korizki@users.noreply.github.com>
2022-11-04 10:02:02 +0800

    Test Update Page Title

--- pages/main.php
+++ pages/main.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sistem Tiket Trakindo - Main</title>
+    <title>Sistem Tiket Trakindo</title>
     <link rel="icon" href="../assets/icon/laptop.png">
     <link rel="stylesheet" href="../assets/style.css">
     <link rel="stylesheet" href="../assets/responsive.css">
